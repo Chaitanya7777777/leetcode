d786b80091a520219a93f6c3515b2d5b89170eb0
Chaitanya Gulechha <cgulechha7@gmail.com>
2026-07-31 13:08:15 +0530

    Time: 5 ms (79.89%), Space: 25.6 MB (61.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        
+        vector<int>freq(26,0);
+        for(char& c:word)freq[c-'a']++;
+        priority_queue<int>pq;
+        for(auto& x:freq){
+            if(x>0)pq.push(x);
+        }
+        int dis=0;
+        int ans=0;
+        while(!pq.empty()){
+            int mul=1+(dis/8);
+            ans+=mul*pq.top();
+            pq.pop();
+            dis++;
+        }
+        return ans;
     }
 };

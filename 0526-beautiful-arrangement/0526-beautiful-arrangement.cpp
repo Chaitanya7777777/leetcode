d24024eb821bca90bb5dc2926b92bf0b85c9a72d
Chaitanya Gulechha <cgulechha7@gmail.com>
2026-08-02 00:14:41 +0530

    Time: 19 ms (72.05%), Space: 17.8 MB (13.05%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-int cnt=0;
-void solve(int n,int pos,vector<bool>&vis){
-    if(pos>n){
-        cnt++;
-        return;
-    }
+int solve(int mask,int n,int pos,vector<vector<int>>&dp){
+    if(mask==(1<<n)-1)return 1;
+    if(dp[mask][pos]!=-1)return dp[mask][pos];
+    int ans=0;
     for(int i=1;i<=n;i++){
-        if(!vis[i]&&(pos%i==0||i%pos==0)){
-            vis[i]=true;
-            solve(n,pos+1,vis);
-            vis[i]=false;
+        int temp=(1<<(i-1));
+        if(!(temp&mask)&&(pos%i==0||i%pos==0)){
+            ans+=solve(temp|mask,n,pos+1,dp);
         }
     }
+    return dp[mask][pos]=ans;
 }
     int countArrangement(int n) {
-        vector<bool>vis(n+1,false);
-        solve(n,1,vis);
-        return cnt;
+        vector<vector<int>>dp(1<<n,vector<int>(n+1,-1));
+        return solve(0,n,1,dp);
     }
 };

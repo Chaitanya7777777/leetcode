eb14b498e788643586ac16a2ef82a611c4baf09c
Chaitanya Gulechha <cgulechha7@gmail.com>
2026-08-11 20:27:08 +0530

    Time: 29 ms (72.66%), Space: 105 MB (53.57%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-int solve(vector<int>&nums,long long int k,int l,int r){
-    long long int x=nums[l];
-    int res=l;
-    while(l<=r){
-        int m=l+(r-l)/2;
-        if(nums[m]<=k*x){
-            res=m;
-            l=m+1;
-        }
-        else r=m-1;
-    }
-    return res;
-}
     int minRemoval(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int ans=INT_MAX;
+        int ans=0;
+        int j=0;
         for(int i=0;i<n;i++){
-            int id=solve(nums,k,i,n-1);
-            ans=min(ans,i+(n-id-1));
+            while((long long int)nums[i]>(long long int)nums[j]*k)j++;
+            ans=max(ans,i-j+1);
         }
-        return ans;
+        return n-ans;
     }
 };

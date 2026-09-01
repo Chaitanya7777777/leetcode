a18840e1d223351ddbafbcd31b87afcb7cb96599
Chaitanya Gulechha <cgulechha7@gmail.com>
2026-09-01 21:52:47 +0530

    Time: 751 ms (60.2%), Space: 311.2 MB (45.92%) - LeetHub

---
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int minMoves(vector<string>& classroom, int energy) {
+        int n=classroom.size(),m=classroom[0].size();
+        map<pair<int,int>,int>id;
+        int sr=-1,sc=-1,cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(classroom[i][j]=='S'){
+                    sr=i;
+                    sc=j;
+                }
+                else if(classroom[i][j]=='L'){
+                    id[{i,j}]=cnt;
+                    cnt++;
+                }
+            }
+        }
+        int totm=(1<<cnt)-1;
+        vector<vector<vector<int>>>best(n,vector<vector<int>>(m,vector<int>(totm+1,-1)));
+        queue<vector<int>>q;
+        q.push({sr,sc,0,energy,0});
+        best[sr][sc][0]=energy;
+        vector<int>dir={0,1,0,-1,0};
+        while(!q.empty()){
+            auto& it=q.front();
+            int r=it[0],c=it[1],lit=it[2],en=it[3],dis=it[4];
+            q.pop();
+            if(lit==totm)return dis;
+            if(en==0)continue;
+            for(int i=0;i<4;i++){
+                int nr=r+dir[i],nc=c+dir[i+1];
+                if(nr<0||nr>=n||nc<0||nc>=m)continue;
+                if(classroom[nr][nc]=='X')continue;
+                int nlit=lit,nen=en-1;
+                if(classroom[nr][nc]=='L'){
+                    nlit=nlit|(1<<(id[{nr,nc}]));
+                }
+                if(classroom[nr][nc]=='R'){
+                    nen=energy;
+                }
+                if(nen>best[nr][nc][nlit]){
+                    best[nr][nc][lit]=nen;
+                    q.push({nr,nc,nlit,nen,dis+1});
+                }
+            }
+        }
+        return -1;
+    }
+};
